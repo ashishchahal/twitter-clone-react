8c447131ddd1747d6787779dbf47a9da89336e9a
Ashish Chahal <ashish.chahal27@gmail.com>
2020-10-06 21:54:07 +0530

    sidebar done with functionality & ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <h2>This is twitter clone</h2>
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Home */}
+
+      {/* Widgets */}
     </div>
   );
 }
