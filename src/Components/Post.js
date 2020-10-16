0020feb3de4e0ab89b2b930efe9cfcb7c9ab7110
Ashish Chahal <ashish.chahal27@gmail.com>
2020-10-17 04:06:38 +0530

    react-flip-move implemented using forwardRefs

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
@@ -7,38 +7,40 @@ import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 import "../Styles/Post.css";
 
-function Post({ displayName, username, verified, text, image, avatar }) {
-  console.log(avatar, "🥑");
-  return (
-    <div className="post">
-      <div className="post__avatar">
-        <Avatar src={avatar} />
-      </div>
-      <div className="post__body">
-        <div className="post__header">
-          <div className="header__post__headerText">
-            <h3>
-              {displayName}{" "}
-              <span className="post__headerSpecial">
-                {verified && <VerifiedUserIcon className="post__badge" />}@
-                {username}
-              </span>
-            </h3>
+const Post = forwardRef(
+  ({ displayName, username, verified, text, image, avatar }, ref) => {
+    console.log(avatar, "🥑");
+    return (
+      <div className="post" ref={ref}>
+        <div className="post__avatar">
+          <Avatar src={avatar} />
+        </div>
+        <div className="post__body">
+          <div className="post__header">
+            <div className="header__post__headerText">
+              <h3>
+                {displayName}{" "}
+                <span className="post__headerSpecial">
+                  {verified && <VerifiedUserIcon className="post__badge" />}@
+                  {username}
+                </span>
+              </h3>
+            </div>
+            <div className="post__headerDescription">
+              <p>{text}</p>
+            </div>
           </div>
-          <div className="post__headerDescription">
-            <p>{text}</p>
+          <img src={image} alt="" />
+          <div className="post__footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon />
+            <FavoriteBorderIcon />
+            <PublishIcon />
           </div>
         </div>
-        <img src={image} alt="" />
-        <div className="post__footer">
-          <ChatBubbleOutlineIcon fontSize="small" />
-          <RepeatIcon />
-          <FavoriteBorderIcon />
-          <PublishIcon />
-        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default Post;
